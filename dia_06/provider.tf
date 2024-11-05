1ab3249bee81c8a8384f34bb81cd9c8989a4a9fc
William <william@DESKTOP-NBO5DC3>
2024-11-04 23:21:27 -0300

    dia 6 aula 1

---
+++ dia_06/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "descomplicando-terraform-william"
+    key    = "aula_for"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+#Configure thw AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
